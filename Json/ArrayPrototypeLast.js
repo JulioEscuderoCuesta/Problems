379a56aeab99cd145acd1375cdbd5da14b6e0b39
Julio Escudero Cuesta <escuderocuestajulio@gmail.com>
2025-12-14 18:41:30 +0100

    Simple. If the array is not empty, calculate the last position from its length.

---
+++ Json/ArrayPrototypeLast.js
@@ -0,0 +1,19 @@
+// Write code that enhances all arrays such that you can call the array.last() 
+// method on any array and it will return the last element. 
+// If there are no elements in the array, it should return -1.
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    /*if (this.length)
+        return this[this.length - 1];
+    return -1*/
+
+    return this.length ? this[this.length - 1] : -1;
+};
+
+const arr = [1 , 2, 3];
+const arr2 = [];
+console.log(arr.last());
+console.log(arr2.last());
